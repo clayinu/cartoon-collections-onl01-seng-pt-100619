24257407130f720b362ec3bd5da5a873ab8897ba
Hoko Le <hokole527@gmail.com>
2019-11-30 23:21:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(array)
+
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |name|
+    "#{name.capitalize}!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |name|
+    if name.size > 4
+      return TRUE
+    else
+      return FALSE
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.include?("cheddar")
+  array.include?("gouda")
+  array.include?("camembert")
 end
